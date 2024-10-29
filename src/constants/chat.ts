128f681ad68f875fb249071a844eb4167d3b5fcf
jakezheng <111893112+jakenzheng@users.noreply.github.com>
2024-10-29 16:18:29 -0500

    Update chat.ts

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -30,26 +30,20 @@ export const modelOptions: ModelOptions[] = [
   'gpt-4-turbo-2024-04-09',
   'gpt-4o',
   'gpt-4o-2024-05-13',
-  'gpt-3.5-turbo-0301',
-  'gpt-4-0314',
-  'gpt-4-32k-0314',
 ];
 
 export const defaultModel = 'gpt-3.5-turbo';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
-  'gpt-3.5-turbo-0301': 4096,
   'gpt-3.5-turbo-0613': 4096,
   'gpt-3.5-turbo-16k': 16384,
   'gpt-3.5-turbo-16k-0613': 16384,
   'gpt-3.5-turbo-1106': 16384,
   'gpt-3.5-turbo-0125': 16384,
   'gpt-4': 8192,
-  'gpt-4-0314': 8192,
   'gpt-4-0613': 8192,
   'gpt-4-32k': 32768,
-  'gpt-4-32k-0314': 32768,
   'gpt-4-32k-0613': 32768,
   'gpt-4-1106-preview': 128000,
   'gpt-4-0125-preview': 128000,
@@ -61,10 +55,6 @@ export const modelMaxToken = {
 
 export const modelCost = {
   'gpt-3.5-turbo': {
-    prompt: { price: 0.0015, unit: 1000 },
-    completion: { price: 0.002, unit: 1000 },
-  },
-  'gpt-3.5-turbo-0301': {
     prompt: { price: 0.0015, unit: 1000 },
     completion: { price: 0.002, unit: 1000 },
   },
@@ -92,19 +82,11 @@ export const modelCost = {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
   },
-  'gpt-4-0314': {
-    prompt: { price: 0.03, unit: 1000 },
-    completion: { price: 0.06, unit: 1000 },
-  },
   'gpt-4-0613': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
   },
   'gpt-4-32k': {
-    prompt: { price: 0.06, unit: 1000 },
-    completion: { price: 0.12, unit: 1000 },
-  },
-  'gpt-4-32k-0314': {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
   },
